b62e3d7f894154418528aeb7edaf7b9eacfbfbfa
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2016-02-24 09:30:35 +0200

    ** Improved code formatting

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 var crypto = require('crypto');
 var shortid = require('shortid');
 
-var decrypt = function(key, iv, encryptdata) {
-  encryptdata = new Buffer(encryptdata, 'base64').toString('binary');
-  
-  var decipher = crypto.createDecipheriv('aes-256-cbc', key, iv),
-      decoded  = decipher.update(encryptdata);
-  
+var decrypt = function (key, iv, encryptdata) {
+  var data = new Buffer(encryptdata, 'base64').toString('binary');
+  var decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+  var decoded = decipher.update(data);
   decoded += decipher.final();
   return decoded;
 };
 
-var encrypt = function(key, iv, cleardata) {
-    var encipher = crypto.createCipheriv('aes-256-cbc', key, iv),
-        encryptdata  = encipher.update(cleardata);
-
-    encryptdata += encipher.final();
-     return new Buffer(encryptdata, 'binary').toString('base64');
+var encrypt = function (key, iv, cleardata) {
+  var encipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+  var encryptdata = encipher.update(cleardata);
+  encryptdata += encipher.final();
+  return new Buffer(encryptdata, 'binary').toString('base64');
 };
 
-var defaultStampGenerator = function(){
+var defaultStampGenerator = function () {
   return shortid.generate();
 };
 
-var OnnistuuClient = function(options){
+var OnnistuuClient = function (options) {
   this.customer_id = options.customer_id;
   this.encryption_key = options.encryption_key;
   this.success_url = options.success_url;
@@ -31,9 +28,9 @@ var OnnistuuClient = function(options){
   this.failure_url = options.failure_url || '/';
   this.onnistuu_url = options.onnistuu_url || 'https://www.onnistuu.fi/external/entry/';
   this.stamp_generator = options.stamp_generator || defaultStampGenerator;
-}
+};
 
-OnnistuuClient.prototype.createRequestBody = function(requirements, document, return_success){
+OnnistuuClient.prototype.createRequestBody = function (requirements, document, return_success) {
   var encryptData = {
     stamp: this.stamp_generator(),
     customer: this.customer_id,
